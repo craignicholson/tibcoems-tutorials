e11302352ed7a91fd244adde722d42026713ee17
Craig Nicholson <craig.nicholson@gmail.com>
2016-12-15 13:58:57 -0600

    Added SendByteMessage to send a file to a consumer through EMS

---
+++ 2_SendByteMessage/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using TIBCO.EMS;
+
+
+class Program
+{
+    string serverUrl = null;
+    string userName = null;
+    string password = null;
+    string name = "ExportQ";
+    string clientID = null;
+    string durableName = "subscriber";
+
+    bool useTopic = true;
+    bool useAsync = false;
+
+    Connection connection = null;
+    Session session = null;
+    MessageProducer msgProducer = null;
+    Destination destination = null;
+
+    //EMSCompletionListener completionListener = null;
+
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("SendByteMessage Server (Sender/Publisher) started - BYTE MESSAGE");
+        new Program().Run();
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private void Run()
+    {
+        try
+        {
+            BytesMessage msg;
+            Console.WriteLine("Publishing to destination '" + name + "'\n");
+
+            ConnectionFactory factory = new TIBCO.EMS.ConnectionFactory("localhost");
+            connection = factory.CreateConnection("", "");
+
+            // create the session
+            session = connection.CreateSession(false, Session.AUTO_ACKNOWLEDGE);
+            destination = session.CreateQueue(name);
+
+            //create the producer
+            msgProducer = session.CreateProducer(null);
+
+            byte[] file = File.ReadAllBytes("Nlog.xml");
+            msg = session.CreateBytesMessage();
+            msg.WriteBytes(file);
+            msg.MsgType = "Byte";
+
+            //Add special properties
+            msg.SetStringProperty("FILE_SIZE", file.Length.ToString());
+            msg.SetStringProperty("FILE_NAME", Guid.NewGuid().ToString());
+
+            //publish the message
+            msgProducer.Send(destination, msg);
+            Console.WriteLine("Published message: " + msg.ToString());
+        }
+        catch (EMSException e)
+        {
+            Console.Error.WriteLine("Exception in csMsgProducer: " + e.Message);
+            Console.Error.WriteLine(e.StackTrace);
+            Environment.Exit(-1);
+        }
+    }
+}
