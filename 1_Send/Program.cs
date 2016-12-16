386b99820c14cffd65ea3266496a74cb76b09b86
Craig Nicholson <craig.nicholson@gmail.com>
2016-12-15 19:22:12 -0600

    formatting code and adding comments for readability

--- 1_Send/Program.cs
+++ 1_Send/Program.cs
@@ -3,9 +3,18 @@ using TIBCO.EMS;
 
 class Program
 {
+    /// <summary>
+    /// For this example make sure you run 1_Receive.exe at least once.  You can start up more than
+    /// one instance of 1_Receive.exe to see the Publisher (1_Send.exe) publish a message to all of
+    /// the running receivers (Subscribers).
+    /// </summary>
+    TopicConnection publisherConnection;
+    TopicSession publisherSession;
+    TopicPublisher emsServerPublisher;
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Producer started");
+        Console.WriteLine("1_Send - Producer (Publisher) started");
         new Program().Run();
         Console.WriteLine("Press [enter] to exit.");
         Console.ReadLine();
@@ -17,13 +26,17 @@ class Program
         EMSServerPublishThisMessage("Hello World", "Owner", "HelloWorld");
     }
 
-    TopicConnection publisherConnection;
-    TopicSession publisherSession;
-    TopicPublisher emsServerPublisher;
+    /// <summary>
+    /// CreateEMSServerTopicPublisher creates the Topic we will need to publish to
+    /// and binds the Topic to a session and creates the publisher.
+    /// </summary>
     private void CreateEMSServerTopicPublisher()
     {
-        TopicConnectionFactory factory = new TIBCO.EMS.TopicConnectionFactory("localhost");
-        publisherConnection = factory.CreateTopicConnection("", ""); // Username, password
+        var topicname = "GeneralTopic";
+        Console.WriteLine("Createing a topic we can publish to: " + topicname);
+
+        TopicConnectionFactory factory = new TopicConnectionFactory("localhost");
+        publisherConnection = factory.CreateTopicConnection("", ""); // Username, password blank for dev instance
         publisherSession = publisherConnection.CreateTopicSession(false, Session.AUTO_ACKNOWLEDGE);
         Topic generalTopic = publisherSession.CreateTopic("GeneralTopic");
         emsServerPublisher = publisherSession.CreatePublisher(generalTopic);
@@ -31,13 +44,18 @@ class Program
         publisherConnection.Start();
     }
 
+    /// <summary>
+    /// EMSServerPublishThisMessage publishes the message to EMS
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="propertyName"></param>
+    /// <param name="propertyValue"></param>
     internal void EMSServerPublishThisMessage(string message, string propertyName, string propertyValue)
     {
         TextMessage textMessage = publisherSession.CreateTextMessage();
         textMessage.Text = message;
         textMessage.SetStringProperty(propertyName, propertyValue);
         emsServerPublisher.Publish(textMessage);
-        Console.WriteLine("EMS Publisher published TEXT message: " + message);
+        Console.WriteLine("EMS Publisher published message: " + message);
     }
 }
-
