4ac6ee3518230698e72799edfb5b22d90b4ec652
Craig Nicholson <craig.nicholson@gmail.com>
2016-12-17 15:19:55 -0600

    Add example which generates tasks

---
+++ 0_SendHelloWorld/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using TIBCO.EMS;
+
+class Program
+{
+    string queueName = "HelloWorldQ";
+    Connection connection = null;
+    Session session = null;
+    MessageProducer msgProducer = null;
+    Destination queue = null;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("SendHelloWorld Producer");
+        new Program().Run();
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private void Run()
+    {
+        try
+        {
+            TextMessage msg;
+            Console.WriteLine("Publishing to queue '" + queueName + "'\n");
+
+            ConnectionFactory factory = new TIBCO.EMS.ConnectionFactory("localhost");
+            connection = factory.CreateConnection("", "");
+
+            // create the session
+            session = connection.CreateSession(false, Session.AUTO_ACKNOWLEDGE);
+            queue = session.CreateQueue(queueName);
+
+            //create the producer
+            msgProducer = session.CreateProducer(null);
+
+            msg = session.CreateTextMessage();
+
+            // load the txt
+            msg.Text = "Hello World";
+
+            //compress
+            msg.SetBooleanProperty("JMS_TIBCO_COMPRESS", true);
+
+            //publish the message
+            msgProducer.Send(queue, msg);
+            Console.WriteLine("Published message: " + msg.ToString());
+        }
+        catch (EMSException e)
+        {
+            Console.Error.WriteLine("Exception in SendHelloWorld : " + e.Message);
+            Console.Error.WriteLine(e.StackTrace);
+            Environment.Exit(-1);
+        }
+    }
+}
---
+++ 4_Tasks/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using TIBCO.EMS;
+
+class Program
+{
+    string queueName = "TasksQ";
+    Connection connection = null;
+    Session session = null;
+    MessageProducer msgProducer = null;
+    Destination queue = null;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Tasks Producer");
+        new Program().Run();
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+
+    private void Run()
+    {
+        Console.WriteLine("Publishing to queue '" + queueName + "'\n");
+        int tasks = 1000;
+
+        // Generate random numbers, more interesting than just i being incremented
+        Random rnd = new Random();
+        try
+        {
+            for (int i=0 ; i < tasks; i++) {
+                TextMessage msg;
+
+                ConnectionFactory factory = new TIBCO.EMS.ConnectionFactory("localhost");
+                connection = factory.CreateConnection("", "");
+
+                // create the session
+                session = connection.CreateSession(false, Session.AUTO_ACKNOWLEDGE);
+                queue = session.CreateQueue(queueName);
+
+                //create the producer
+                msgProducer = session.CreateProducer(null);
+
+                msg = session.CreateTextMessage();
+                var number = rnd.Next(1000,1000000);
+
+                // update console
+                Console.WriteLine("Number : " + number);
+
+                // load the txt - which is a number we want to now the primes
+                // note: receiver uses Convert.ToInt32 so we need to only pass in valid number.
+                msg.Text = number.ToString();
+
+                //Another options is to use a property
+                msg.SetIntProperty("number", number);
+
+                //compress
+                //msg.SetBooleanProperty("JMS_TIBCO_COMPRESS", true);
+
+                //publish the message
+                msgProducer.Send(queue, msg);
+                Thread.Sleep(100);
+            }
+            Console.WriteLine("Tasks Sent : " + tasks.ToString());
+        }
+        catch (EMSException e)
+        {
+            Console.Error.WriteLine("Exception in Tasks : " + e.Message);
+            Console.Error.WriteLine(e.StackTrace);
+            Environment.Exit(-1);
+        }
+    }
+}
